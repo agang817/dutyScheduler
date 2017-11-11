e39480d8a96b543c8beafb0ff768a9f43eaf8558
agang <aegang817@gmail.com>
2017-11-11 18:19:48 -0500

    implementation of residentAssistant class, TODO: Add calendar to ResidentAssistant

--- resident_assistant.h
+++ resident_assistant.h
@@ -10,27 +10,27 @@
 using namespace std;
 
 class ResidentAssistant{
-	//Stores and maintains information regarding an RA's availability and preferences
-	public:
-		ResidentAssistant();
-		void initRA(string name, int prefs[]);
-		void setName(string name);
-		string getName();
-		void setGoalPts(int pts);
-		int getGoalPts();
-		void setCurrentPts(int pts);
-		int getCurrentPts();
-		void setSelectionScore(int score);
-		int getSelectionScore();
-		int getPref(int weekDay);
-		void setPref(int weekDay, int pref);
+//Stores and maintains information regarding an RA's availability and preferences
+public:
+	ResidentAssistant();
+	void initRA(string name, int prefs[]);
+	void setName(string name);
+	string getName();
+	void setGoalPts(int pts);
+	int getGoalPts();
+	void setCurrentPts(int pts);
+	int getCurrentPts();
+	void setSelectionScore(int score);
+	int getSelectionScore();
+	int getPref(int weekDay);
+	void setPref(int weekDay, int pref);
 
-	private:
-		string RAname;
-		int preferences[7] = {0};
-		int pointGoal;
-		int currentPoints = 0;
-		int selectionScore = 0;
+private:
+	string RAname;
+	int preferences[7] = {0};
+	int pointGoal;
+	int currentPoints = 0;
+	int selectionScore = 0;
 };
 
 #endif
